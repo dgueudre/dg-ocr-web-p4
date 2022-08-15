fbaa26c54728cfee8054ce4724fc14126ac8149d
dgueudre <dgueudre@gmail.com>
2022-08-15 13:09:30 +0200

    setup eleventy

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+module.exports = config => {
+  config.addPassthroughCopy('src/css');
+  config.addPassthroughCopy('src/font');
+  config.addPassthroughCopy('src/img');
+  config.addPassthroughCopy('src/includes');
+  config.addPassthroughCopy('src/js');
+
+  config.addPassthroughCopy('src/favicon.jpg');
+  config.addPassthroughCopy('src/style.css');
+
+  config.addNunjucksFilter("filter_by", (array, property, value) => array.filter(item => item[property] === value));
+  config.addNunjucksFilter("sort_by", (array, property) => array.sort((a, b) => a[property] - b[property]));
+
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'public',
+      includes: "templates",
+      data: "data"
+    }
+  };
+};
